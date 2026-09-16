cd981dae52ae9e3ab3f4f531678f8a515431337e
prabukprajith28 <prabukprajith28@gmail.com>
2026-09-16 18:11:49 +0530

    "Update file name"

--- tests/demo.spec.ts
+++ tests/demo.spec.ts
@@ -7,6 +7,8 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
+
+
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
